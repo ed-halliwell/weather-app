f2577543cbe446a225d6085163012cc0fd288f7b
Ed Halliwell <edhalliwell@hotmail.com>
2021-11-12 16:38:21 +0000

    Update http calls to https

--- src/utils/APIFragments.ts
+++ src/utils/APIFragments.ts
@@ -1,17 +1,17 @@
 // Weather API docs: https://openweathermap.org/current
 export const WEATHER_API_BASE_URL =
-  "http://api.openweathermap.org/data/2.5/weather?q=";
+  "https://api.openweathermap.org/data/2.5/weather?q=";
 export const WEATHER_API_KEY = "&appid=2aeb3fa9d22eee3b7631e9db2332118e";
 
 export const WEATHER_FORECAST_API_BASE_URL =
-  "http://api.openweathermap.org/data/2.5/onecall?";
+  "https://api.openweathermap.org/data/2.5/onecall?";
 
 // Geo API docs: https://docs.mapbox.com/api/search/geocoding/#reverse-geocoding
 export const GEO_API_BASE_URL =
   "https://api.mapbox.com/geocoding/v5/mapbox.places/";
 
 export const GEO_CODING_BASE_URL =
-  "http://api.openweathermap.org/geo/1.0/reverse?";
+  "https://api.openweathermap.org/geo/1.0/reverse?";
 
 export const GEO_API_KEY =
   ".json?access_token=pk.eyJ1IjoiZWRoYWxsaXdlbGwiLCJhIjoiY2trdXozZ2N4MWNyaDJ2cGMweWRvcDk1ZiJ9.ExEuJTASuQGJOm2oDnGSjw";
